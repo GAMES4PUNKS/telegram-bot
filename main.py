71224c66ec9f03f53abff6ac75292dc72f3afa75
GAMES4PUNKS <sercullen@googlemail.com>
2025-08-03 15:48:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import nest_asyncio
 import os
+import nest_asyncio
 
-# Enable nested event loop (Render compatibility)
+# Enable nested event loop
 nest_asyncio.apply()
 
-# BOT TOKEN from environment
+# Load Bot Token from ENV
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Check for token existence
 if not BOT_TOKEN:
-    raise ValueError("No BOT_TOKEN found in environment variables!")
+    raise ValueError("BOT_TOKEN not set in environment variables!")
 
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+# Enable Logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Command Handlers
@@ -26,28 +23,19 @@ def start(update, context):
 def status(update, context):
     update.message.reply_text("GK Games4PUNKS Studio is LIVE, purchase a Game Key NFT to play: https://neftyblocks.com/collection/games4punks1/drops")
 
-# Error handler
+# Error Handler
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
-    # Create the Updater and pass it your bot's token.
     updater = Updater(BOT_TOKEN, use_context=True)
-
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # Commands
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("status", status))
-
-    # Log all errors
     dp.add_error_handler(error)
 
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you send a signal (Ctrl+C)
     updater.idle()
 
 if __name__ == '__main__':
